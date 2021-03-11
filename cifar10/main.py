cc4b061c4e8ba20bbba2766be0af1458361885e0
Francois de VIEILLEVILLE <francois.devieilleville@agenium.com>
2021-03-12 00:45:35 +0100

    clean

--- cifar10/main.py
+++ cifar10/main.py
@@ -269,9 +269,6 @@ def log_basic_info(logger, config):
         logger.info("\nDistributed setting:")
         logger.info(f"\tbackend: {idist.backend()}")
         logger.info(f"\tworld size: {idist.get_world_size()}")
-        logger.info("\n")
-
-
 
 def create_trainer(model, optimizer, criterion, lr_scheduler, train_sampler, config, logger):
 
